56eee272ec39f748e4d80e061f30fbae9d706a0b
eugene.vashchilin <77799844+V3ugen@users.noreply.github.com>
2022-08-04 10:43:47 +0300

    second Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import  throttle  from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function play ({seconds}) {
+  localStorage.setItem('videoplayer-current-time', seconds)
+  console.log(seconds);
+};
+
+player.on('timeupdate', throttle(play, 1000))
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
